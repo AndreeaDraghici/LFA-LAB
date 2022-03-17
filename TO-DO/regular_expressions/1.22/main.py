adb4685a2b2ca90d7bf6d47b198948e0b48c1ca2
AndreeaDraghici <draghici108@gmail.com>
2022-03-17 21:31:22 +0200

    feat: added to do 1.22

---
+++ TO-DO/regular_expressions/1.22/main.py
@@ -0,0 +1,18 @@
+import re
+
+
+def regex() :
+    reg = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain
+        r'localhost|'  # localhost
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # IP
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    string = "https://pytutorial.com"
+    print(re.match(reg, string) is not None)
+
+
+if __name__ == '__main__' :
+    regex()
