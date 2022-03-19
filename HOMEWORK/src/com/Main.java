da9731914e1ec4259939a9255f18aa7ddac7c352
Drăghici Andreea-Maria <draghici108@gmail.com>
2022-03-19 21:51:12 +0200

    Add files via upload

---
+++ HOMEWORK/src/com/Main.java
@@ -0,0 +1,14 @@
+package com;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Controller window = new Controller();
+        window.frame.setVisible(true);
+        window.frame.setResizable(false);
+
+    }
+}
