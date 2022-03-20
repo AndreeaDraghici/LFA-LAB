583a7a8c1400124b8de83dea92dc09072f87bbee
Drăghici Andreea-Maria <draghici108@gmail.com>
2022-03-20 19:14:05 +0200

    chore: updated controller

--- HOMEWORK/src/com/Tema1/Controller.java
+++ HOMEWORK/src/com/Tema1/Controller.java
@@ -26,24 +26,22 @@ public class Controller {
     private JRadioButton caseSensitive;
     private boolean caseChoice;
 
-    /**
-     * Create the application.
-     */
+    /** Create the application.*/
     public Controller() {
         initialize();
     }
 
-    /**
-     * Initialize the contents of the frame.
-     */
+    /** Initialize the contents of the frame.*/
     private void initialize() {
 
+        /** create a new frame */
         frame = new JFrame("Regular Expression");
         frame.getContentPane().setBackground(new Color(241, 202, 144));
         frame.setBounds(600, 250, 750, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);
 
+        /** create a label to display text */
         JLabel labelRegularExpression = new JLabel("Enter your regex");
         labelRegularExpression.setBounds(37, 50, 300, 36);
         frame.add(labelRegularExpression);
@@ -53,6 +51,7 @@ public class Controller {
         frame.getContentPane().add(regularExpression);
         regularExpression.setColumns(10);
 
+        /** create a label to display text */
         JLabel labelTestString = new JLabel("Test string");
         labelTestString.setBounds(37, 120, 300, 36);
         frame.add(labelTestString);
@@ -62,6 +61,7 @@ public class Controller {
         frame.getContentPane().add(testString);
         testString.setColumns(10);
 
+        /** create a label to display text */
         JLabel labelMatchInfo = new JLabel("Match information");
         labelMatchInfo.setBounds(350, -7, 216, 148);
         frame.add(labelMatchInfo);
@@ -74,7 +74,14 @@ public class Controller {
         frame.getContentPane().add(scroll);
 
         caseSensitive = new JRadioButton("Case sensitive");
-        caseSensitive.addActionListener(arg0 -> caseChoice = !caseSensitive.isSelected());
+        caseSensitive.addActionListener(arg0 -> {
+            if (!caseSensitive.isSelected()){
+                caseChoice = true;
+            }
+            else {
+                caseChoice = false;
+            }
+        });
         caseSensitive.setBounds(130, 220, 150, 30);
         caseSensitive.setSelected(true);
         caseSensitive.setBackground(new Color(241, 202, 144));
@@ -87,12 +94,34 @@ public class Controller {
             string = testString.getText();
 
             if (!regex.isEmpty() && !string.isEmpty()) {
+
+                /** Defines a pattern */
                 final Pattern pattern;
+
+                /**
+                 * if you need to match a text against a regular expression pattern more than one time,
+                 * you need to create a Pattern instance using the Pattern.compile() method
+                 *
+                 * Case Insensitive enables case-insensitive matching, the case of letters will be ignored when performing a search
+                 *
+                 * In multiline mode the expressions ^ and $ match just after or just before,
+                 * respectively, a line terminator or the end of the input sequence
+                 *
+                 **/
                 if (caseChoice) {
                     pattern = Pattern.compile(regex, Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
                 } else {
                     pattern = Pattern.compile(regex, Pattern.MULTILINE);
                 }
+
+                /**
+                 * Used to search for the pattern
+                 *
+                 * matcher() method is used to search for the pattern in a string
+                 *
+                 * find() method returns true if the pattern was found in the string and false if it was not found
+                 *
+                 * */
                 final Matcher matcher = pattern.matcher(string);
                 matchInformation.setText(null);
 
@@ -101,6 +130,13 @@ public class Controller {
                     matchInformation.append("\n\nYour regular expression does not match the subject string.\n\n");
                 } else {
                     matchInformation.setForeground(Color.BLUE);
+                    /**
+                     * If multiple matches can be found in the text, the find() method will find the first,
+                     * and then for each subsequent call to find() it will move to the next match
+                     *
+                     * The methods start() and end() will give the indexes into the text where the found match starts and ends
+                     *
+                     * */
                     matchInformation.append("\nMatch 1 " + matcher.start() + "-" + matcher.end() + " " + matcher.group(0) + "\n");
                     int i = 2;
                     while (matcher.find()) {
