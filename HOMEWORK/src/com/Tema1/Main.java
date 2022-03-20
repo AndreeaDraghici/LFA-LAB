f0a38105a729ae04a727264f65fe6751644c682d
Drăghici Andreea-Maria <draghici108@gmail.com>
2022-03-20 16:00:31 +0200

    chore: updated main

--- HOMEWORK/src/com/Tema1/Main.java
+++ HOMEWORK/src/com/Tema1/Main.java
@@ -1,14 +1,30 @@
-package com;
+package com.Tema1;
 
+import java.awt.*;
+
+/**
+ * author: andreea draghici
+ * 2022
+ * Regex , LFA LAB
+ */
 
 public class Main {
 
+    /**
+     * Launch the application.
+     */
 
     public static void main(String[] args) {
 
-        Controller window = new Controller();
-        window.frame.setVisible(true);
-        window.frame.setResizable(false);
-
+        EventQueue.invokeLater(() -> {
+            try {
+                Controller window = new Controller();
+                window.frame.setVisible(true);
+                window.frame.setResizable(false);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
+
 }
