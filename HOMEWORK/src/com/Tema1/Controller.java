155b723108c7d621ee5d860a346ccd8343d59181
Drăghici Andreea-Maria <draghici108@gmail.com>
2022-03-21 20:54:24 +0200

    feat: code refactoring

--- HOMEWORK/src/com/Tema1/Controller.java
+++ HOMEWORK/src/com/Tema1/Controller.java
@@ -2,6 +2,7 @@ package com.Tema1;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,14 +13,20 @@ import java.util.regex.Pattern;
  */
 public class Controller {
 
-    public JFrame frame;
+    private JFrame frame;
     private JTextField regularExpression;
     private JTextField testString;
     private JTextArea matchInformation;
-    private String regex;
-    private String string;
     private JRadioButton caseSensitive;
     private boolean caseChoice;
+
+    public JFrame getFrame() {
+        return frame;
+    }
+
+    public void setFrame(JFrame frame) {
+        this.frame = frame;
+    }
 
     /**
      * Create the application.
@@ -33,136 +40,143 @@ public class Controller {
      */
     private void initialize() {
 
-        /** create a new frame */
-        frame = new JFrame("Regular Expression");
-        frame.getContentPane().setBackground(new Color(241, 202, 144));
-        frame.setBounds(600, 250, 750, 400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setLayout(null);
+        /* create a new frame */
+        setFrame(new JFrame("Regular Expression"));
+        getFrame().getContentPane().setBackground(new Color(241, 202, 144));
+        getFrame().setBounds(600, 250, 750, 400);
+        getFrame().setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        getFrame().getContentPane().setLayout(null);
 
-        /** create a label to display text */
+        /* create a label to display text */
         JLabel labelRegularExpression = new JLabel("Enter your regex");
         labelRegularExpression.setBounds(37, 50, 300, 36);
-        frame.add(labelRegularExpression);
+        getFrame().add(labelRegularExpression);
 
         regularExpression = new JTextField();
         regularExpression.setBounds(37, 85, 300, 30);
-        frame.getContentPane().add(regularExpression);
+        getFrame().getContentPane().add(regularExpression);
         regularExpression.setColumns(10);
 
-        /** create a label to display text */
+        /* create a label to display text */
         JLabel labelTestString = new JLabel("Test string");
         labelTestString.setBounds(37, 120, 300, 36);
-        frame.add(labelTestString);
+        getFrame().add(labelTestString);
 
         testString = new JTextField();
         testString.setBounds(37, 155, 300, 65);
-        frame.getContentPane().add(testString);
+        getFrame().getContentPane().add(testString);
         testString.setColumns(10);
 
-        /** create a label to display text */
+        /* create a label to display text */
         JLabel labelMatchInfo = new JLabel("Match information");
         labelMatchInfo.setBounds(350, -7, 216, 148);
-        frame.add(labelMatchInfo);
+        getFrame().add(labelMatchInfo);
 
         matchInformation = new JTextArea();
         matchInformation.setEditable(false);
         JScrollPane scroll = new JScrollPane(matchInformation);
         matchInformation.setBounds(350, 85, 353, 200);
         scroll.setBounds(350, 85, 356, 206);
-        frame.getContentPane().add(scroll);
+        getFrame().getContentPane().add(scroll);
 
         caseSensitive = new JRadioButton("Case sensitive");
         caseSensitive.addActionListener(arg0 -> {
-            caseChoice = !caseSensitive.isSelected();
+            if (caseSensitive.isSelected()) {
+                caseChoice = false;
+            } else {
+                caseChoice = true;
+            }
         });
         caseSensitive.setBounds(130, 220, 150, 30);
         caseSensitive.setSelected(true);
         caseSensitive.setBackground(new Color(241, 202, 144));
-        frame.add(caseSensitive);
+        getFrame().add(caseSensitive);
 
         JButton btnSubmit = new JButton("Extract the pattern");
-        btnSubmit.addActionListener(arg0 -> {
+        btnSubmit.addActionListener(this::actionButtonSubmit);
 
-            regex = regularExpression.getText();
-            string = testString.getText();
-            try {
-                if (!regex.isEmpty() && !string.isEmpty()) {
+        btnSubmit.setBounds(50, 260, 150, 29);
+        getFrame().getContentPane().add(btnSubmit);
 
-                    /** Defines a pattern */
-                    final Pattern pattern;
+        JButton btnClear = new JButton("Clear");
+        btnClear.addActionListener(this::actionButtonClear);
 
-                    /**
-                     * if you need to match a text against a regular expression pattern more than one time,
-                     * you need to create a Pattern instance using the Pattern.compile() method
-                     *
-                     * Case Insensitive enables case-insensitive matching, the case of letters will be ignored when performing a search
-                     *
-                     * In multiline mode the expressions ^ and $ match just after or just before,
-                     * respectively, a line terminator or the end of the input sequence
-                     *
-                     **/
+        btnClear.setBounds(200, 260, 106, 29);
+        getFrame().getContentPane().add(btnClear);
+    }
 
-                    if (caseChoice) {
-                        pattern = Pattern.compile(regex, Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
-                    } else {
-                        pattern = Pattern.compile(regex, Pattern.MULTILINE);
-                    }
+    private void actionButtonSubmit(ActionEvent arg0) {
 
-                    /**
-                     * Used to search for the pattern
-                     *
-                     * matcher() method is used to search for the pattern in a string
-                     *
-                     * find() method returns true if the pattern was found in the string and false if it was not found
-                     *
-                     * */
-                    final Matcher matcher = pattern.matcher(string);
-                    matchInformation.setText(null);
+        String regex = regularExpression.getText();
+        String str = testString.getText();
+        try {
+            if (!regex.isEmpty() && !str.isEmpty()) {
+                /* Defines a pattern */
+                final Pattern pattern;
+                /*
+                  if you need to match a text against a regular expression pattern more than one time,
+                  you need to create a Pattern instance using the Pattern.compile() method
 
+                  Case Insensitive enables case-insensitive matching, the case of letters will be ignored when performing a search
+
+                  In multiline mode the expressions ^ and $ match just after or just before,
+                  respectively, a line terminator or the end of the input sequence
+
+                 */
+                if (caseChoice) {
+                    pattern = Pattern.compile(regex, Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
+                } else {
+                    pattern = Pattern.compile(regex, Pattern.MULTILINE);
+                }
+                /*
+                  Used to search for the pattern
+
+                  matcher() method is used to search for the pattern in a string
+
+                  find() method returns true if the pattern was found in the string and false if it was not found
+
+                  */
+                final Matcher matcher = pattern.matcher(str);
+                matchInformation.setText(null);
+                try {
                     if (!matcher.find()) {
                         matchInformation.setForeground(Color.RED);
                         matchInformation.append("\n\nYour regular expression does not match the subject string.\n\n");
                     } else {
                         matchInformation.setForeground(Color.BLUE);
-                        /**
-                         * If multiple matches can be found in the text, the find() method will find the first,
-                         * and then for each subsequent call to find() it will move to the next match
-                         *
-                         * The methods start() and end() will give the indexes into the text where the found match starts and ends
-                         *
-                         * */
+                        /*
+                          If multiple matches can be found in the text, the find() method will find the first,
+                          and then for each subsequent call to find() it will move to the next match
+
+                          The methods start() and end() will give the indexes into the text where the found match starts and ends
+
+                          */
                         matchInformation.append("\nMatch 1 " + matcher.start() + "-" + matcher.end() + " " + matcher.group(0) + "\n");
-                        int i = 2;
+                        int value = 2;
                         while (matcher.find()) {
-                            matchInformation.append("\nMatch " + i + matcher.start() + "-" + matcher.end() + " " + matcher.group(0) + "\n");
-                            i = i + 1;
+                            matchInformation.append("\nMatch  " + value + " " + matcher.start() + "-" + matcher.end() + " " + matcher.group(0) + "\n");
+                            value = value + 1;
                         }
-                        if (i-- == 1) {
-                            matchInformation.append("\n   " + i + " match ");
+                        if (value == 1) {
+                            matchInformation.append("\n " + value + " match ");
                         } else {
-                            matchInformation.append("\n   " + i + " matches ");
+                            matchInformation.append("\n " + value + " matches ");
                         }
+                        value--;
                     }
-
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
                 }
-            } catch (Exception exception) {
-                JOptionPane.showMessageDialog(null, "Error: " + exception.getLocalizedMessage());
             }
-        });
-
-        btnSubmit.setBounds(50, 260, 150, 29);
-        frame.getContentPane().add(btnSubmit);
-
-        JButton btnClear = new JButton("Clear");
-        btnClear.addActionListener(arg0 -> {
-            matchInformation.setText(null);
-            regularExpression.setText(null);
-            testString.setText(null);
-        });
-
-        btnClear.setBounds(200, 260, 106, 29);
-        frame.getContentPane().add(btnClear);
-
+        } catch (Exception exception) {
+            JOptionPane.showMessageDialog(null, "Error: " + exception.getMessage());
+        }
     }
+
+    private void actionButtonClear(ActionEvent arg0) {
+        matchInformation.setText(null);
+        regularExpression.setText(null);
+        testString.setText(null);
+    }
+
 }
