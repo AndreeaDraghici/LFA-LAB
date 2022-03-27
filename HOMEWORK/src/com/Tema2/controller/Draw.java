3270efa096ffe51604d9930e3856619a046b3bed
Drăghici Andreea-Maria <draghici108@gmail.com>
2022-03-27 14:52:47 +0300

    chore: controller updated

--- HOMEWORK/src/com/Tema2/controller/Draw.java
+++ HOMEWORK/src/com/Tema2/controller/Draw.java
@@ -13,12 +13,15 @@ import java.io.Serial;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import static java.awt.Color.BLACK;
+import static java.awt.Color.WHITE;
+
 public class Draw extends JPanel implements MouseListener, MouseMotionListener {
 
     @Serial
     private static final long serialVersionUID = 1L;
     private String action;
-    private String info;
+    private String information;
     private final Graph graph;
     private boolean selectedEdge;
     private String currentVertex;
@@ -97,16 +100,16 @@ public class Draw extends JPanel implements MouseListener, MouseMotionListener {
 
         g.setFont(new Font("Arial", Font.PLAIN, 20));
         dosD.fillRect(0, 0, this.getWidth(), this.getHeight());
-        g.setColor(Color.WHITE);
+        g.setColor(WHITE);
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
 
         this.drawVertices(g);
 
         g.setFont(new Font("Arial", Font.PLAIN, 30));
-        g.setColor(Color.BLACK);
+        g.setColor(BLACK);
         g.drawString(this.action, 10, 25);
         g.setFont(new Font("Arial", Font.PLAIN, 15));
-        g.drawString(info, 10, 40);
+        g.drawString(information, 10, 40);
     }
 
     //display the info depending on the action selected and update current action
@@ -114,19 +117,19 @@ public class Draw extends JPanel implements MouseListener, MouseMotionListener {
         this.action = action;
         if ("Vertex".equals(action)) {
             this.resetVerticesColors();
-            this.info = "Click on the screen to add vertices";
+            this.information = "Click on the screen to add vertices";
         } else if ("Edge".equals(action)) {
             this.resetVerticesColors();
-            this.info = "Click on vertices to join them";
+            this.information = "Click on vertices to join them";
         } else if ("Drag".equals(action)) {
-            this.info = "Click on a vertex and drag it across the screen";
+            this.information = "Click on a vertex and drag it across the screen";
         } else if ("Delete".equals(action)) {
-            this.info = "Click on a vertex to remove it";
+            this.information = "Click on a vertex to remove it";
         }
         this.repaint();
     }
 
-    public void mouseReleased(MouseEvent e) {
+    public void mouseReleased(MouseEvent event) {
         if (this.action.equals("Drag")) {
             this.resetVerticesColors();
             this.currentVertex = "";
@@ -134,19 +137,123 @@ public class Draw extends JPanel implements MouseListener, MouseMotionListener {
     }
 
     // search for the vertex by coordinates and update them
-    public void mouseDragged(MouseEvent e) {
+    public void mouseDragged(MouseEvent event) {
 
         if (this.action.equals("Drag")) {
             if (this.currentVertex.equals("")) {
-                String foundVertexKey = this.searchByCoordinates(e.getX(), e.getY());
+                String foundVertexKey = this.searchByCoordinates(event.getX(), event.getY());
                 if (foundVertexKey == null) {
                     return;
                 }
                 this.currentVertex = foundVertexKey;
             }
-            this.updateCoordinates(this.currentVertex, e.getX(), e.getY());
+            this.updateCoordinates(this.currentVertex, event.getX(), event.getY());
             this.repaint();
         }
+    }
+
+    public void drawVertices(Graphics graphics) {
+
+        Vertex vertex = this.graph.getHead();
+        if (vertex == null) {
+            return;
+        }
+        this.drawEdges(graphics);
+        while (vertex != null) {
+            graphics.setColor(vertex.getColor());
+            graphics.fillOval(vertex.getxCoord(), vertex.getyCoord(), vertex.getzCoord(), vertex.getzCoord());
+            graphics.setColor(BLACK);
+            graphics.drawString(vertex.getKey(), vertex.getxCoord(), vertex.getyCoord() - 10);
+            vertex = vertex.getNextVertex();
+        }
+    }
+
+    private void drawEdges(Graphics graphics) {
+
+        Vertex head = this.graph.getHead();
+        if (head == null) {
+            return;
+        }
+        Vertex vertex;
+        graphics.setColor(BLACK);
+        while (head != null) {
+            vertex = head.getAdjacentVertex();
+            while (vertex != null && graph.getVertexMap().containsKey(vertex.getKey())) {
+                graphics.drawString(vertex.getEdgeKey(), this.getAverageDistance(head.getxCoord(), vertex.getxCoord()), this.getAverageDistance(head.getyCoord(), vertex.getyCoord()));
+                graphics.drawLine(vertex.getxCoord(), vertex.getyCoord(), head.getxCoord() + (this.graph.getSIZE() / 2), head.getyCoord() + (this.graph.getSIZE() / 2));
+                vertex = vertex.getNextVertex();
+            }
+            head = head.getNextVertex();
+        }
+    }
+
+    // searches a vertex by its coordinates and returns its key if found
+    public String searchByCoordinates(int xCoordinate, int yCoordinate) {
+        Vertex head = this.graph.getHead();
+        if (head == null) {
+            return null;
+        }
+        if (xCoordinate > head.getxCoord() && xCoordinate < head.getxCoord() + this.graph.getSIZE() && yCoordinate > head.getyCoord() && yCoordinate < head.getyCoord() + this.graph.getSIZE()) {
+            head.setColor(Color.RED);
+            return head.getKey();
+        } else {
+            head = head.getNextVertex();
+        }
+        while (head != null) {
+            if (xCoordinate > head.getxCoord() && xCoordinate < head.getxCoord() + this.graph.getSIZE() && yCoordinate > head.getyCoord() && yCoordinate < head.getyCoord() + this.graph.getSIZE()) {
+                head.setColor(Color.RED);
+                return head.getKey();
+            } else {
+                head = head.getNextVertex();
+            }
+        }
+        return null;
+    }
+
+    public void resetVerticesColors() {
+
+        Vertex head = this.graph.getHead();
+        if (head == null) {
+            return;
+        }
+        while (head != null) {
+            head.setColor(new Color(0xd9820f));
+            head = head.getNextVertex();
+        }
+    }
+
+    public void resetColor(String key) {
+        if (this.graph.getVertexMap().containsKey(key)) {
+            Vertex node = this.graph.getVertexMap().get(key);
+            node.setColor(new Color(0xd9820f));
+            this.repaint();
+        }
+    }
+
+    public void updateCoordinates(String key, int xCoordinate, int yCoordinate) {
+        Vertex head = this.graph.getHead();
+        while (head != null) {
+            if (head.getKey().equals(key)) {
+                head.setX(xCoordinate - this.graph.getSIZE() / 2);
+                head.setY(yCoordinate - this.graph.getSIZE() / 2);
+            }
+            this.updateVertexCoordinates(head.getAdjacentVertex(), key, xCoordinate, yCoordinate);
+            head = head.getNextVertex();
+        }
+    }
+
+    public void updateVertexCoordinates(Vertex vertex, String key, int  xCoordinate, int yCoordinate) {
+        while (vertex != null) {
+            if (vertex.getKey().equals(key)) {
+                vertex.setX( xCoordinate);
+                vertex.setY(yCoordinate);
+            }
+            vertex = vertex.getNextVertex();
+        }
+    }
+
+    public int getAverageDistance(int xPoint, int yPoint) {
+        return (xPoint + ((xPoint - yPoint) / 2) * -1);
     }
 
     public void mousePressed(MouseEvent e) {
@@ -165,107 +272,5 @@ public class Draw extends JPanel implements MouseListener, MouseMotionListener {
         // TODO document why this method is empty
     }
 
-    public void drawVertices(Graphics g) {
 
-        Vertex vertex = this.graph.getHead();
-        if (vertex == null) {
-            return;
-        }
-        this.drawEdges(g);
-        while (vertex != null) {
-            g.setColor(vertex.getColor());
-            g.fillOval(vertex.getX(), vertex.getY(), vertex.getZ(), vertex.getZ());
-            g.setColor(Color.BLACK);
-            g.drawString(vertex.getKey(), vertex.getX(), vertex.getY() - 10);
-            vertex = vertex.getNext();
-        }
-    }
-
-    private void drawEdges(Graphics g) {
-
-        Vertex head = this.graph.getHead();
-        if (head == null) {
-            return;
-        }
-        Vertex vertex;
-        g.setColor(Color.BLACK);
-        while (head != null) {
-            vertex = head.getAdjacentVertex();
-            while (vertex != null && graph.getVertexMap().containsKey(vertex.getKey())) {
-                g.drawString(vertex.getEdgeKey(), this.getAverageDistance(head.getX(), vertex.getX()), this.getAverageDistance(head.getY(), vertex.getY()));
-                g.drawLine(vertex.getX(), vertex.getY(), head.getX() + (this.graph.getSIZE() / 2), head.getY() + (this.graph.getSIZE() / 2));
-                vertex = vertex.getNext();
-            }
-            head = head.getNext();
-        }
-    }
-
-    // searches a vertex by its coordinates and returns its key if found
-    public String searchByCoordinates(int x, int y) {
-        Vertex head = this.graph.getHead();
-        if (head == null) {
-            return null;
-        }
-        if (x > head.getX() && x < head.getX() + this.graph.getSIZE() && y > head.getY() && y < head.getY() + this.graph.getSIZE()) {
-            head.setColor(Color.RED);
-            return head.getKey();
-        } else {
-            head = head.getNext();
-        }
-        while (head != null) {
-            if (x > head.getX() && x < head.getX() + this.graph.getSIZE() && y > head.getY() && y < head.getY() + this.graph.getSIZE()) {
-                head.setColor(Color.RED);
-                return head.getKey();
-            } else {
-                head = head.getNext();
-            }
-        }
-        return null;
-    }
-
-    public void resetVerticesColors() {
-
-        Vertex head = this.graph.getHead();
-        if (head == null) {
-            return;
-        }
-        while (head != null) {
-            head.setColor(new Color(217, 130, 15));
-            head = head.getNext();
-        }
-    }
-
-    public void resetColor(String key) {
-        if (this.graph.getVertexMap().containsKey(key)) {
-            Vertex node = this.graph.getVertexMap().get(key);
-            node.setColor(new Color(217, 130, 15));
-            this.repaint();
-        }
-    }
-
-    public void updateCoordinates(String key, int x, int y) {
-        Vertex head = this.graph.getHead();
-        while (head != null) {
-            if (head.getKey().equals(key)) {
-                head.setX(x - this.graph.getSIZE() / 2);
-                head.setY(y - this.graph.getSIZE() / 2);
-            }
-            this.updateVertexCoordinates(head.getAdjacentVertex(), key, x, y);
-            head = head.getNext();
-        }
-    }
-
-    public void updateVertexCoordinates(Vertex vertex, String key, int x, int y) {
-        while (vertex != null) {
-            if (vertex.getKey().equals(key)) {
-                vertex.setX(x);
-                vertex.setY(y);
-            }
-            vertex = vertex.getNext();
-        }
-    }
-
-    public int getAverageDistance(int px, int py) {
-        return (px + ((px - py) / 2) * -1);
-    }
 }
